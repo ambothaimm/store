1ba596d27260b7241cd4b09d02fff3ee99ab6588
ambothaimm <166310689+ambothaimm@users.noreply.github.com>
2024-04-07 22:20:27 +0800

    Add files via upload

---
+++ store_uuu/src/inventory/SimpleDateFormat.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inventory;
+
+import java.util.Date;
+
+/**
+ *
+ * @author admin
+ */
+class SimpleDateFormat {
+
+    SimpleDateFormat(String yyyyMMdd) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    Date parse(String dateString) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
